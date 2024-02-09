c2313c60918f5fd125ba4075dfe0796182270b7d
assisfaavf <assisfaavfp@gmail.com>
2024-02-09 13:06:06 -0300

    Expressões e Operadores

---
+++ Nível_4/expressoes_e_operadores.js
@@ -0,0 +1,46 @@
+// new
+let name = new String("assis");
+let age = new Number(23);
+
+console.log(name, age);
+
+/*Operadores unários
+typeOf
+delete
+*/
+//typeOf
+const person = {
+  name: "assis",
+  age: 23,
+};
+console.log(person);
+console.log(typeof person);
+delete person.age;
+console.log(person);
+
+// Operadores Aritiméticos
+// Multiplicação
+console.log(3 * 4);
+// Divisão
+console.log(5 / 2);
+// Adição
+console.log(3 + 5);
+// Subtração
+console.log(2 - 1);
+
+// Resto da divisão (%)
+
+let remainder = 11 % 3;
+console.log(remainder);
+// incremento (++)
+let increment = 0;
+console.log(increment);
+increment++;
+console.log(increment);
+// deecremento (--)
+let decrement = 1;
+console.log(decrement);
+increment--;
+console.log(decrement);
+// Exponencial (**)
+console.log(3 ** 2);
