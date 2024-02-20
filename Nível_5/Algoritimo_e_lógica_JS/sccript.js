64212a9cd5b24d93fdda9b9ce882819faac76e9d
assisfaavf <assisfaavfp@gmail.com>
2024-02-20 16:27:08 -0300

    Nível 5 aula 4

--- Nível_5/Algoritimo_e_lógica_JS/sccript.js
+++ Nível_5/Algoritimo_e_lógica_JS/sccript.js
@@ -17,10 +17,38 @@ Pergunte o nome do usuário e escreva a seguinte mensagem:
 */
 
 // Solução:
+// alert("Iremos fazer a soma de dois números informados por você");
+// let num11 = prompt("Digite o primeiro número");
+// let num21 = prompt("Digite o segundo número");
+// alert("O resultado da soma dos dois número informados é: " + (Number(num11) + Number(num21)))
 
-let num11 = prompt("Digite o primeiro número");
-let num21 = prompt("Digite o segundo número");
-let num1 = Number(num11);
-let num2 = Number(num21);
-let result = num1 + num2;
-alert(`O resultado da soma dos dois número informados é: ${result}`);
+// __________________________________________________________
+
+/*
+3) Capurar dois número e realizar as seguintes operações:
+-Soma
+-Subtração
+-Multiplicação
+-Divisão
+-Resto da divisão
+E para cada operação, apresentar o resultado final ao usuário
+*/
+
+// Solução:
+// alert("Iremos fazer a soma, subtração, multiplicação, divisão e resto da divisão de dois números informados por você");
+//  let numberOne = prompt("Digite o primeiro número");
+//  let numberTwo = prompt("Digite o segundo número");
+
+//  let sum = Number(numberOne) + Number(numberTwo);
+//  let sub = Number(numberOne) - Number(numberTwo);
+//  let mult = Number(numberOne) * Number(numberTwo);
+//  let div = Number(numberOne) / Number(numberTwo);
+//  let rest = Number(numberOne) % Number(numberTwo);
+
+//  alert(`O resultado da soma dos números informados é: ${sum}`);
+//  alert(`O resultado da subtração dos números informados é: ${sub}`);
+//  alert(`O resultado da multiplicação dos números informados é: ${mult}`);
+//  alert(`O resultado da divisão dos números informados é: ${div}`);
+//  alert(`O resultado do resto da divisão dos números informados é: ${rest}`);
+// __________________________________________________________
+
