ed150e337692f606547453afe6c464a889ebe55c
assisfaavf <assisfaavfp@gmail.com>
2024-02-20 13:22:12 -0300

    aula 3 nível 5

---
+++ Nível_5/Algoritimo_e_lógica_JS/sccript.js
@@ -0,0 +1,26 @@
+/*
+1) Encontre a solução para o seguinte problema:
+Pergunte o nome do usuário e escreva a seguinte mensagem:
+"Olá, [Nome do usuário]"
+*/
+
+// Solução:
+
+// let usuário = prompt("Qual o seu nome ?");
+
+// alert(`Olá, ${usuário}`);
+
+//__________________________________________________________
+
+/*
+2) Solicite dois número e faça a soma deles e apresente o resultado final ao usuário
+*/
+
+// Solução:
+
+let num11 = prompt("Digite o primeiro número");
+let num21 = prompt("Digite o segundo número");
+let num1 = Number(num11);
+let num2 = Number(num21);
+let result = num1 + num2;
+alert(`O resultado da soma dos dois número informados é: ${result}`);
