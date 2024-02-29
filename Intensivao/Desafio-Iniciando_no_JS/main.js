303ffcc578ca1458a8762dc399a3492d3c007eec
assisfaavf <assisfaavfp@gmail.com>
2024-02-29 12:54:28 -0300

    intensivão concuído

---
+++ Intensivao/Desafio-Iniciando_no_JS/main.js
@@ -0,0 +1,127 @@
+// 1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+
+// Solução
+
+// alert("Hello World!");
+
+//______________________________________________________________________________________________________________________________________________
+
+// 2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+
+// Solução
+
+// let numberOne = 2;
+// let numberTwo = 1;
+// let sum = numberOne + numberTwo;
+// alert(sum);
+
+//______________________________________________________________________________________________________________________________________________
+
+// 3. Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+
+// Solução
+
+// let variable = "das";
+
+// if (typeof variable == "number") {
+//   alert("É um número!");
+// } else {
+//   alert("Não é um número!");
+// }
+
+//______________________________________________________________________________________________________________________________________________
+
+// 4. Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+
+// Solução
+
+// let variable = 3;
+
+// if (typeof variable == "string") {
+//   alert("É uma string!");
+// } else {
+//   alert("Não é uma string!");
+// }
+
+//______________________________________________________________________________________________________________________________________________
+
+// 5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+
+// Solução
+
+// let variable = false;
+
+// if (typeof variable == "boolean") {
+//   alert("É um booleano!");
+// } else {
+//   alert("Não é um booleano!");
+// }
+
+//______________________________________________________________________________________________________________________________________________
+
+// 6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+
+// Solução
+
+// let numberOne = 1;
+// let numberTwo = 2;
+
+// let subtraction = numberOne - numberTwo;
+
+// alert(subtraction);
+
+//______________________________________________________________________________________________________________________________________________
+
+// 7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+
+// Solução
+
+// let numberOne = 14;
+// let numberTwo = 2;
+
+// let multiplication = numberOne * numberTwo;
+
+// alert(multiplication);
+
+//______________________________________________________________________________________________________________________________________________
+
+// 8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+
+// Solução
+
+// let numberOne = 14;
+// let numberTwo = 2;
+
+// let division = numberOne / numberTwo;
+
+// alert(division);
+
+//______________________________________________________________________________________________________________________________________________
+
+// 9. Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+
+// Solução
+
+// let variable = 3;
+
+// if (variable % 2 == 0) {
+//   alert("É um número par!");
+// } else {
+//   alert("Não é um número par!");
+// }
+
+//______________________________________________________________________________________________________________________________________________
+
+// 10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+
+// Solução
+
+// let variable = 259;
+
+// if (variable % 2 != 0) {
+//   alert("É um número impar!");
+// } else {
+//   alert("Não é um número impar!");
+// }
+
+//______________________________________________________________________________________________________________________________________________
