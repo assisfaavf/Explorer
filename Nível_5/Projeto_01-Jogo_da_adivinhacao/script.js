710c7cd25e295edf2e64f9f332e289cba07dcd2f
assisfaavf <assisfaavfp@gmail.com>
2024-03-05 13:11:28 -0300

    Modificando DOM

---
+++ Nível_5/Projeto_01-Jogo_da_adivinhacao/script.js
@@ -0,0 +1,7 @@
+function handleClick(event) {
+  event.preventDefault();
+  console.log("Teste");
+
+  const inputNumber = document.querySelector("#inputNumber");
+  console.log(inputNumber.value);
+}
