71d6c8c24d22908bde99c4c3dd369e97a726771d
assisfaavf <assisfaavfp@gmail.com>
2024-03-12 20:15:54 -0300

    porojeto 3

---
+++ Nível_5/Projeto_03-FocusTimer/js/FocusTimer/events.js
@@ -0,0 +1,11 @@
+import {controls} from "./elements.js"
+
+
+
+
+export function registerControls () {
+  controls.addEventListener("click", (event) => {
+    
+    console.log(event.target.dataset.action)
+  })
+}
