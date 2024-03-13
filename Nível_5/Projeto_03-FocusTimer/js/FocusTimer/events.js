9e0e1c774d6de71ce6c3d187bbd71db9eea80395
assisfaavf <assisfaavfp@gmail.com>
2024-03-13 15:43:19 -0300

    não sei oque ta errado

--- Nível_5/Projeto_03-FocusTimer/js/FocusTimer/events.js
+++ Nível_5/Projeto_03-FocusTimer/js/FocusTimer/events.js
@@ -1,11 +1,13 @@
-import {controls} from "./elements.js"
+import { controls } from "./elements.js";
+import * as actions from "./actions.js";
 
-
-
-
-export function registerControls () {
+export function registerControls() {
   controls.addEventListener("click", (event) => {
-    
-    console.log(event.target.dataset.action)
-  })
+    const action = event.target.dataset.action;
+    if (typeof actions[action] != "function") {
+      // return;
+    }
+    console.log(action);
+    actions[`"${action}"`]();
+  });
 }
--- Nível_5/Projeto_03-FocusTimer/js/FocusTimer/index.js
+++ Nível_5/Projeto_03-FocusTimer/js/FocusTimer/index.js
@@ -1,12 +1,9 @@
-import state from "./state.js"
-import * as events from "./events.js"
+import state from "./state.js";
+import * as events from "./events.js";
 
+export function start(minutes, seconds) {
+  state.minutes = minutes;
+  state.seconds = seconds;
 
-
-
-
-export function start (minutes, seconds) {
-  state.minutes = minutes
-  state.seconds = seconds
-
+  events.registerControls();
 }
