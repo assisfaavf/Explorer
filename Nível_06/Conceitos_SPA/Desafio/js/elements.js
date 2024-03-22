7361c13b6c90fb513e83187bd7bf91e3cefab9d3
assisfaavf <assisfaavfp@gmail.com>
2024-03-22 13:04:57 -0300

    desafio 1 nível 6 em andanmento

---
+++ Nível_06/Conceitos_SPA/Desafio/js/elements.js
@@ -0,0 +1,5 @@
+const bg_home = document.querySelector(".home");
+const bg_universe = document.querySelector(".universe");
+const bg_exploration = document.querySelector(".exploration");
+
+export { bg_home, bg_exploration, bg_universe };
